46dd1051befc418da5ed3f3e644b7ca55fec6fd4
brunocesarangst <bruno.angst@rede.ulbra.br>
2023-03-28 14:49:39 -0300

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
+import React from 'react';
 import { ThemeProvider } from "next-themes";
-import ButtonChangeTheme from "./buttonChangeTheme";
+import ButtonChangeTheme from "./ButtonChangeTheme";
 
 export default function Home() {
   return (
     <ThemeProvider attribute='class'>
-        <ButtonChangeTheme />
+      <ButtonChangeTheme />
       <h1 className="text-3xl font-bold underline">
-      Hello, Next.js!
+        Hello, Next.js!
       </h1>
     </ThemeProvider>
   )
